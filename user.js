ea6d7f2b76d0477b77a8cd3479311e0ec70629b9
seun jay <seunjay92@gmail.com>
2019-04-17 09:01:32 +0100

    Add file system module to user.js file

---
+++ user.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+
+function User(name, email, password, id, status){
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.id = id;
+  this.status = status
+};
