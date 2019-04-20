496029fbf91c10d1b256e91f3c2fe3f785af4316
seun jay <seunjay92@gmail.com>
2019-04-20 21:20:48 +0100

    Remove some commented lines of code in user.test.js file

--- user.test.js
+++ user.test.js
@@ -4,49 +4,6 @@ const db = require('./db.json');
 let femi = new User('femi ajayi', 'femi@gmail.com', 9845, 'admin');
 
 
-
-
-// test('Check to see that name and email are both strings', () => {
-
-//   let seun = new User(23, 34, '1234', 'admin');
-//   expect(seun).toBe('Invalid Input');
-// });
-
-// test('Check to see that User constructor function is defined', () => {
-
-//   let seun = new User('Seun Jay', 'seunjay@gmail.com', 1234, 'admin');
-//   expect(seun.name).toBe('Seun Jay');
-// });
-
-
-// test('Check to see that a user has been created', () => {
-//   let newlyCreatedUser = [];
-//   let newUser = new User("John Doe", "joe@gmail.com", 4321, "user");
-//   //let obj = newUser.createUser();
-//   expect(newUser.createUser()).toBe('Your user account has been successfully created');
-
-// });
-
-// test("should check if a  user can be checked with his name and status", () => {
-//   let result = [];
-//   let newUser = new User("John Doe", "joe@gmail.com", 4321, "user");
-//   let search = newUser.createUser();
-//   expect(newUser.searchUser("John Doe")).toEqual({ id: 1, name: 'John Doe', email: 'joe@gmail.com', password: 4321, status: 'user' });
-//   //expect(result).toBeDefined();
-// });
-
-// test("read user by ID", () =>{
-//   let femi = new User('femi ajayi', 'femi@gmail.com', 9845, 'admin');
-//   let jamesUser = femi.createUser();
-//   expect(femi.readSingleUser(3)).toEqual({id: 3, name: 'femi ajayi', email: 'femi@gmail.com', password: 9845, status: 'admin'})
-// });
-
-// test("read user by ID", () =>{
-//   let femi = new User('femi ajayi', 'femi@gmail.com', 9845, 'admin');
-//   let jamesUser = femi.createUser();
-//   expect(femi.readSingleUser(5)).toBe('ID not valid');
-// });
-
 describe("Testing the functionalities of a user", () => {
 
   test('Check to see that User constructor function is defined', () => {
@@ -73,7 +30,6 @@ describe("Testing the functionalities of a user", () => {
   });
 
   test("read user by ID", () =>{
-    //let femi = new User('femi ajayi', 'femi@gmail.com', 9845, 'admin');
     let seun = new User('Seun Jay', 'seunjay@gmail.com', 1234, 'admin');
     expect(seun.readSingleUser(1)).toEqual({id: 1, name: 'Seun Jay', email: 'seunjay@gmail.com', password: 1234, status: 'admin'})
   });
@@ -85,13 +41,11 @@ describe("Testing the functionalities of a user", () => {
   });
 
   test("search user by name", () =>{
-    //let femi = new User('femi ajayi', 'femi@gmail.com', 9845, 'admin');
     let seun = new User('Seun Jay', 'seunjay@gmail.com', 1234, 'admin');
     expect(seun.searchUser('Seun Jay')).toEqual({id: 1, name: 'Seun Jay', email: 'seunjay@gmail.com', password: 1234, status: 'admin'})
   });
 
   test("Should return false if the name is not a registered name", () =>{
-    //let femi = new User('femi ajayi', 'femi@gmail.com', 9845, 'admin');
     let seun = new User('Seun Jay', 'seunjay@gmail.com', 1234, 'admin');
     expect(seun.searchUser('Seun')).toBe(false);
   });
@@ -109,44 +63,3 @@ describe("Testing the functionalities of a user", () => {
 
 
 
-// describe("To test if all normal user activities", () => {
-//   test("Should check instance of user", () => {
-//       let newUser = new Users("w", "w@gmail.com", "w", "user");
-//       expect(newUser).toEqual({username: 'w', email: 'w@gmail.com', password: 'w', access: 'user' });
-//   });
-//   test("should check if an admin account can be successfully created", () => {
-//       let newUser = new Users("whitehox", "white@gmail.com", "123", "admin");
-//       let success = newUser.createUser();
-//       expect(success).toBe("Your administrator account has been successfully created");
-//   });
-//   test("Should check if a user account can be successfully created", () => {
-//       let newUser = new Users("whitehox", "white@gmail.com", "123", "user");
-//       let success = newUser.createUser();
-//       expect(success).toBe("Your user account has been successfully created");
-//   });
-//   test("Should return an error message if a wrong account type was used", () => {
-//       let newUser = new Users("whitehox", "white@gmail.com", "123", "foreign");
-//       let error = newUser.createUser();
-//       expect(error).toBe("A user can not be created with this access level. Kindly use either admin or user");
-//   });
-//   test("should check if a  user can be checked with his ID", () => {
-//       let result;
-//       let search = Users.prototype.searchSingleUserById(3, "admin");
-//       result = search;
-//       console.log(result);
-//       expect(result).toEqual([dbData.admin[0]]);
-//       expect(result).toBeDefined();
-//   });
-//   // test("Should check if a user account was successfully updated", () => {
-//   //     let response = Users.prototype.updateUser("michael", "11861538da", "michael", "oketegah@gmail.com", "11861538da", "user");
-//   //     expect(response).toBe("Your user account has been successfully updated");
-//   // });
-//   test("should check if a user entered right username or password", () => {
-//       let response = Users.prototype.updateUser("kolokolokolo", "11861538da", "whitehox", "oketegah@gmail.com", "11861538da", "user");
-//       expect(response).toBe("Incorrect username or password");
-//   });
-//   test("should check if a admin entered right username or password", () => {
-//       let response = Users.prototype.updateUser("kolokolokolo", "11861538da", "whitehox", "oketegah@gmail.com", "11861538da", "admin");
-//       expect(response).toBe("Incorrect username or password");
-//   });
-// });
