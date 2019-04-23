c6b877fc093241c3d1729db272cec26faeb5cb0f
seun jay <seunjay92@gmail.com>
2019-04-23 15:51:59 +0100

    Restore deleted orders.js file

---
+++ orders.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const db = require('./db.json');
+let User = require('./user');
+
+function Order(){
+ 
+}
+
+Order.createOrder = function(id){
+  let date = new Date();
+    this.id = id;
+    this.timeOfOrder = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    this.dateOfOrder = `${date.getDate()} : ${date.getMonth()} : ${date.getFullYear()}`;
+
+    if(db.orders.length === 0) {
+      this.id = 1
+    }
+    else {
+      this.id = (db.orders[db.orders.length - 1].id) + 1
+    }
+
+    let orderObject = {
+      id: this.id,
+      timeOfOrder: this.timeOfOrder,
+      dateOfOrder: this.dateOfOrder
+    }
+
+    return orderObject
+
+}
+
+
+
+module.exports = Order;
